f0d66ebeba7b70f2582381f8bd244317ace55fce
AustinChiu <44009612+AustinChiu@users.noreply.github.com>
2019-10-21 15:09:59 -0700

    day 1

--- StudentRecordTester.java
+++ StudentRecordTester.java
@@ -12,8 +12,11 @@ public class StudentRecordTester
         int[] a={50,50,20,80,30};
         int[] b={20,50,50,53,80};
         int[] c={20,50,50,80};
-        
         StudentRecord[] students;
+        students = new StudentRecord [3];
+        students[0] = new StudentRecord(a);
+        students[1] = new StudentRecord(b);
+        students[2]= new StudentRecord(c);
     }
     
 }
